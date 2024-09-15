587104e302dd140775840bf0d392156a16254f81
DESKTOP-470B37N\Admin <phetkla1992@gmail.com>
2024-09-15 19:05:18 +0700

    Exam 1

--- 1_find_tailing_zero/main.py
+++ 1_find_tailing_zero/main.py
@@ -20,4 +20,25 @@ output = number can not be negative
 class Solution:
 
     def find_tailing_zeroes(self, number: int) -> int | str:
+        #Verify non-negative data
+        if number < 0:
+            return "number can not be negative"
+        
+        #Find zeroes factorial
+        count = 0
+        divisor = 5
+        while number >= divisor:
+            count += number // divisor
+            divisor *= 5
+
+        return count
+        
         pass
+
+solution = Solution()
+
+#Input 
+number = int(input("Enter a number : "))
+
+#Call Function
+print("count of tailing zero as an integer :", solution.find_tailing_zeroes(number))
